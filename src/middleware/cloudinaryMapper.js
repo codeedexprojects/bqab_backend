0462669563e9c3efe9d07430c5d191324acb2d1f
Abijithkk <kkabhijith59@gmail.com>
2025-11-24 10:52:26 +0530

    added si no in user section

--- src/middleware/cloudinaryMapper.js
+++ src/middleware/cloudinaryMapper.js
@@ -1,19 +1,18 @@
+
 module.exports = (req, res, next) => {
-  try {
-    if (req.files?.length > 0) {
-      req.body.images = req.files.map((file) => ({
-        url: file.path,
-        public_id: file.filename,
-      }));
-    }
-
-    if (req.file) {
-      req.body.image = req.file.path;
-    }
-
-    next();
-  } catch (error) {
-    next(error);
+  if (req.files && req.files.length > 0) {
+    req.body.images = req.files.map((file) => ({
+      url: file.path,       
+      public_id: file.filename,
+    }));
   }
-};
+
+  if (req.file) {
+    req.body.image = {
+      url: req.file.path,
+      public_id: req.file.filename,
+    };
+  }
+
+  next();
 };
